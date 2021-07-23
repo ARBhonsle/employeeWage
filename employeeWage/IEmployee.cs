9814ec2833b552063c65b94637b9423e80b733c2
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-07-23 19:57:12 +0530

    [aditi] Refactor: Employee monthly wage calculated

--- employeeWage/IEmployee.cs
+++ employeeWage/IEmployee.cs
@@ -6,13 +6,6 @@ namespace employeeWage
 {
     interface IEmployee 
     {
-        public abstract void  EmployeeAttendance();
-        public abstract void EmployeePartAttendance();
-        public abstract void EmployeeDailyWage();
-        public abstract void EmployeePartTimeCalc();
-        public abstract void EmployeePartTimeCalcSwitch();
-        public abstract void EmployeePartWage();
-        public abstract void EmployeePartWageSwitch();
-        public abstract void EmployeeMonthlyWageCalc();
+        public abstract void EmployeeWage();
     }
 }
